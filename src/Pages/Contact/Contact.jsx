9c5901f727a3c757a65501e80a7e5ff651e5dc80
Manmeet Dhingra <manmeetdhingra@googlemail.com>
2023-10-25 22:08:22 +0100

    final

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,16 +3,14 @@ import "./Contact.css"
 
 function Contact() {
   return (
-    <div>
+    <div className="contact-container">
       <form className="form-container">
         <h1 style={{marginTop: "30px", fontSize: "45px"}}>Let's Connect!</h1>
-
         <input type="text" placeholder='Name'/>
         <input type="text" placeholder='Email'/>
         <textarea className="message" type="text" placeholder='Message...'/>
         <input className="submit-btn" type="button" value="Submit" />
       </form>
-      
     </div>
   )
 }
